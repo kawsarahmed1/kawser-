7a2d46767773beb348b9a771f1abdc82180287d3
kawsarahmed1 <61902089+kawsarahmed1@users.noreply.github.com>
2020-03-12 21:35:24 +0600

    Add files via upload

---
+++ class4.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+	$a=10;
+	$a+=20;
+	echo $a	;
+
+
+
+
+
+	
+	?>
+	
+
+<?php
+$x = 10;  
+$y = "100";
+
+if($x == $y){
+	echo "true";
+}else{
+	echo "false";
+};
+echo "</br>";
+
+$x = 100;  
+$y = "100";
+
+var_dump($x === $y); // returns false because types are not equal
+
+$x = 100;  
+$y = 50;
+
+if ($x == 100 and $y == 50) {
+    echo "Hello world!";
+} 
+echo "<br>";
+$txt1="kawsar";
+$txt2="ahmed";
+echo $txt1 . $txt2;
+
+echo "<br>";
+$a=66;
+$b=55;
+if ($a<$b) {
+	echo "a is large";
+}else{
+	echo "b is large";
+}
+
+echo "<br>";
+$a="kawsar";
+$b="ahmed";
+$a.=$b;
+echo $a;
+
+
+
+?>  
+
+
+
+</body>
+</html>
